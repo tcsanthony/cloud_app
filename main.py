102ab031287064d782de4c1305a223e5a6c81459
tcsanthony <57692883+tcsanthony@users.noreply.github.com>
2020-01-10 11:16:00 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+
+app=Flask(__name__)
+
+loaded_model = pickle.load(open("c:/Users/antho/Documents/FTDS/Data Engineering/Deploying/Web/bostonhousinglinmodel.pkl","rb"))
+
+loaded_scaler_model = pickle.load(open("c:/Users/antho/Documents/FTDS/Data Engineering/Deploying/Web/scalermodel.pkl","rb"))
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+def ValuePredictor(to_predict_list):
+    to_predict = np.array(to_predict_list).reshape(1,3)  
+    to_predict = loaded_scaler_model.transform(to_predict)
+    print(to_predict)
+    result = loaded_model.predict(to_predict)
+    return result[0]
+
+@app.route('/result',methods = ['POST'])
+def result():
+    prediction=''
+    if request.method == 'POST':
+        to_predict_list = request.form.to_dict()
+        print(to_predict_list.values())
+        to_predict_list=list(to_predict_list.values())
+        to_predict_list = list(map(float, to_predict_list))
+        print("Before sending to model", to_predict_list)
+        result = ValuePredictor(to_predict_list)
+        print("result from model", result)
+        prediction = result[0]
+        print(prediction)
+        return render_template("result.html",prediction=prediction)
+
+if __name__ == "__main__":
+    app.run(debug=True)
